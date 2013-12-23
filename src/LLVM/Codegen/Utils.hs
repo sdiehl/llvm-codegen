996e480f92a785690f9937a2f739167c627c1a7f
Stephen Diehl <stephen.m.diehl@gmail.com>
2013-12-23 14:47:40 -0600

    env variables

--- src/LLVM/Codegen/Utils.hs
+++ src/LLVM/Codegen/Utils.hs
@@ -1,11 +1,22 @@
 module LLVM.Codegen.Utils where
 
+import Data.Maybe
 import Data.Bits
+
 import Foreign.Ptr (Ptr)
 import Foreign.Storable (sizeOf)
+
+import System.Environment
+import Control.Applicative
 
 bitsize :: Int
 bitsize = bitSize (undefined :: Int)
 
 ptrsize :: Int
 ptrsize = sizeOf (undefined :: Ptr a)
+
+getCC :: IO String
+getCC = fromMaybe "gcc" <$> lookup "CC" <$> getEnvironment
+
+getLD :: IO String
+getLD = fromMaybe "ld" <$> lookup "LD" <$> getEnvironment
