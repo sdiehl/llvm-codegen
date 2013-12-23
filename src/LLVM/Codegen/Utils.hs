16054505b7a64854b1299d5aaefda89abeeb971e
Stephen Diehl <stephen.m.diehl@gmail.com>
2013-12-23 14:36:43 -0600

    size queries

--- src/LLVM/Codegen/Utils.hs
+++ src/LLVM/Codegen/Utils.hs
@@ -1,1 +1,11 @@
 module LLVM.Codegen.Utils where
+
+import Data.Bits
+import Foreign.Ptr (Ptr)
+import Foreign.Storable (sizeOf)
+
+bitsize :: Int
+bitsize = bitSize (undefined :: Int)
+
+ptrsize :: Int
+ptrsize = sizeOf (undefined :: Ptr a)
